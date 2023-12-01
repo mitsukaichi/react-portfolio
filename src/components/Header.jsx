0259d67bc1a0c30bb96c3484b93d65be060f7698
Minami Itsukaichi <mitukaichi@gmail.com>
2023-12-01 10:23:01 -0800

    minor styling changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,8 @@ import Navigation from './Navigation';
 
 export default function Header() {
     return (
-        <div className="container">
         <header className="d-flex justify-content-center py-3">
           <Navigation />
         </header>
-      </div>
     );
   }
