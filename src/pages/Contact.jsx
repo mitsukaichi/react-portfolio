50b88f9db23c4134bdd08b2d958aba1ee6fef70d
Minami Itsukaichi <mitukaichi@gmail.com>
2023-12-01 16:44:06 -0800

    add contact and resume page content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,21 @@
 export default function Contact() {
     return (
-      <div>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+      <div className="d-flex align-items-center justify-content-center">
+        <form className="col-md-6">
+          <div className="form-group my-4">
+            <label >Name</label>
+            <input type="text" className="form-control" id="inputName" placeholder="Enter your name" />
+          </div>
+          <div className="form-group my-4">
+            <label >Email</label>
+            <input type="email" className="form-control" id="inputEmail1" placeholder="Enter your email" />
+          </div>
+          <div className="form-group my-4">
+            <label >Message</label>
+            <textarea type="email" className="form-control" id="inputEmail1" placeholder="Enter your message"></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
       </div>
     );
   }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,49 @@
 export default function Resume() {
-    return (
-      <div>
-        <p>
-          To be updated
-        </p>
+  return (
+    <div className="d-flex flex-wrap justify-content-around align-items-center">
+      <div className="card col-md-5 m-2 shadow p-1 bg-white rounded">
+          <div className="row no-gutters">
+            <div className="card-body">
+                <h4 className="card-title">Minami Itsukaichi</h4>
+                <p className="card-text">Program Manager, Quality, Trust and Safety<br/>
+                  <small className="text-muted">Mountain View, California, United States</small>
+                </p>
+                <br/>
+                <h5 className="card-title">Experience</h5>
+                <p className="card-text">Program Manager at Google<br/>
+                  <small className="text-muted">May 2022 - Present</small>
+                </p>
+                <p className="card-text">Senior Solutions Consultant, Play Books, gTech at Google<br/>
+                  <small className="text-muted">Sep 2018 - May 2022</small>
+                </p>
+                <h5 className="card-title">Education</h5>
+                <p className="card-text">UC Berkeley Extension Full Stuck Web Developer Coding Bootcamp<br/>
+                  <small className="text-muted">Sep 2023 - Dec 2023</small>
+                </p>
+                <p className="card-text">The University of Tokyo<br/>
+                <small className="text-muted">Bachelor of Arts (B.A.)</small>
+                  <small className="text-muted">2005 - 2009</small>
+                </p>
+            </div>
+          </div>
       </div>
-    );
-  }
+      <div className="card col-md-5 m-2 shadow p-1 bg-white rounded">
+          <div className="row no-gutters">
+            <div className="card-body">
+                <h4 className="card-title text-center">Skills</h4>
+                <ul className="align-items-center list-unstyled">
+                  <li className="ms-5">HTML</li>
+                  <li className="ms-5">CSS</li>
+                  <li className="ms-5">Javascript</li>
+                  <li className="ms-5">Node.js</li>
+                  <li className="ms-5">React</li>
+                  <li className="ms-5">MongoDB</li>
+                  <li className="ms-5">MySQL</li>
+                  <li className="ms-5">Express.js</li>
+                </ul>
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+}
